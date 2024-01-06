44418219aacd622b1710ab440d499eb03c1999e0
getoarmorina3 <getoaarm1@gmail.com>
2024-01-07 00:54:05 +0100

    error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,56 +1,75 @@
-"use server";
+'use server';
 
-import { supabase } from "./db";
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
-import { z } from "zod";
+import { supabase } from './db';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { z } from 'zod';
+
+export interface State {
+  errors?: {
+    task?: string[] | undefined;
+  };
+  message?: string | null;
+}
 
 const schema = z.object({
-  task: z.string().min(3, { message: "Task must be at least 3 characters." }),
+  task: z
+    .string({ invalid_type_error: 'Please enter a task' })
+    .min(3, { message: 'Task must be at least 3 characters.' })
+    .max(48, { message: 'Task must be less than 48 characters.' }),
 });
 
-export const createTodo = async (formData: FormData) => {
+export const createTodo = async (prevState: State, formData: FormData) => {
   // Validate form using Zod
-  const parsed = schema.parse({
-    task: formData.get("task"),
+  const validatedFields = schema.safeParse({
+    task: formData.get('task'),
   });
+
+  // Return early if the form data is invalid
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to add to do.',
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { task } = validatedFields.data;
 
   // Insert data into the database
   try {
-    await supabase.from("todos").insert({ task: parsed.task });
-
-    formData = new FormData();
+    await supabase.from('todos').insert({ task });
   } catch (e) {
-    console.error("Transaction failed: ", e);
+    console.error('Transaction failed: ', e);
   }
 
   // Revalidate the cache for the todo page and redirect the user.
-  revalidatePath("/");
-  redirect("/");
+  revalidatePath('/');
+  redirect('/');
 };
 
 export const updateTodo = async (id: number, is_complete: boolean) => {
   // Update data in the database
   try {
-    await supabase.from("todos").update({ is_complete }).match({ id: id });
+    await supabase.from('todos').update({ is_complete }).match({ id: id });
   } catch (e) {
-    console.error("Transaction failed: ", e);
+    console.error('Transaction failed: ', e);
   }
 
   // Revalidate the cache for the todo page and redirect the user.
-  revalidatePath("/");
-  redirect("/");
+  revalidatePath('/');
+  redirect('/');
 };
 
 export const deleteTodo = async (id: number) => {
   // Delete the todo from the database
   try {
-    await supabase.from("todos").delete().match({ id: id });
+    await supabase.from('todos').delete().match({ id: id });
   } catch (e) {
-    console.error("Transaction failed: ", e);
+    console.error('Transaction failed: ', e);
   }
 
   // Revalidate the cache for the todo page and redirect the user.
-  revalidatePath("/");
-  redirect("/");
+  revalidatePath('/');
+  redirect('/');
 };
--- app/ui/add-task.tsx
+++ app/ui/add-task.tsx
@@ -1,27 +1,52 @@
-"use client";
+'use client';
 
-import { createTodo } from "@/app/lib/actions";
-import { useFormStatus } from "react-dom";
+import { State, createTodo } from '@/app/lib/actions';
+import clsx from 'clsx';
+import { useFormStatus, useFormState } from 'react-dom';
 
 export default function AddTask() {
+  let initialState: State = { message: null, errors: {} };
+  const [state, dispatch] = useFormState(createTodo, initialState);
   const { pending } = useFormStatus();
 
+  const isError = state?.errors?.task && state.errors.task.length > 0;
+
   return (
-    <form action={createTodo} className="flex items-center">
-      <input
-        className="flex-grow py-2 px-4 bg-transparent rounded-md bg-white focus:outline-none"
-        id="task"
-        name="task"
-        placeholder="Add a new task..."
-      />
-      <button
-        type="submit"
-        className="ml-4 py-2 px-4 rounded-md bg-slate-900 hover:bg-slate-800 text-white"
-        aria-disabled={pending}
-      >
-        Add
-      </button>
-      <span className="sr-only">Add Todo</span>
-    </form>
+    <>
+      <form action={dispatch} className="flex items-center">
+        <input
+          className={clsx(
+            'flex-grow py-2 px-4 bg-transparent rounded-md bg-white focus:outline-none',
+            {
+              'border border-red-500': isError,
+            }
+          )}
+          id="task"
+          name="task"
+          placeholder="Add a new task..."
+        />
+
+        <button
+          type="submit"
+          className="ml-4 py-2 px-4 rounded-md bg-slate-900 hover:bg-slate-800 text-white"
+          aria-disabled={pending}
+        >
+          Add
+        </button>
+
+        <span className="sr-only">Add Todo</span>
+      </form>
+      {isError && (
+        <div
+          id="task-error"
+          aria-live="polite"
+          className="my-2 pl-3 text-sm text-red-500"
+        >
+          {state.errors?.task?.map((error: string) => (
+            <p key={error}>{error}</p>
+          ))}
+        </div>
+      )}
+    </>
   );
 }
